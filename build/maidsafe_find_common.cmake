b14bc05cb085477dd6e90db4c3256de6ec272cdd
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2011-03-28 11:54:49 +0100

    Initial changes to use common CMake modules.

--- build/maidsafe_find_common.cmake
+++ build/maidsafe_find_common.cmake
@@ -1,49 +1,65 @@
-# ============================================================================ #
+#==============================================================================#
 #                                                                              #
-# Copyright [2011] Sigmoid Solutions Ltd                                       #
+# Copyright [2011] maidsafe.net limited                                        #
 #                                                                              #
-# Description:  Module used to locate MaidSafe-Common tools, cmake modules     #
-#               and the maidsafe_common libs and headers.                      #
-# Created:      2010-12-29                                                     #
-# Company:      Sigmoid Solutions Ltd                                          #
+# Description:  See below.                                                     #
+# Version:      1.0                                                            #
+# Created:      2011-03-20-23.22.31                                            #
+# Revision:     none                                                           #
+# Company:      maidsafe.net limited                                           #
 #                                                                              #
-# The following source code is property of Sigmoid Solutions Ltd and is not    #
+# The following source code is property of maidsafe.net limited and is not     #
 # meant for external use.  The use of this code is governed by the license     #
 # file LICENSE.TXT found in the root of this directory and also on             #
-# www.sigmoidsolutions.com                                                     #
+# www.maidsafe.net.                                                            #
 #                                                                              #
 # You are not free to copy, amend or otherwise use this source code without    #
-# the explicit written permission of the board of directors of Sigmoid         #
-# Solutions.                                                                   #
+# the explicit written permission of the board of directors of maidsafe.net.   #
 #                                                                              #
-# ============================================================================ #
+#==============================================================================#
+#                                                                              #
+#  Written by maidsafe.net team                                                #
+#                                                                              #
+#==============================================================================#
+#                                                                              #
+#  Module used to locate MaidSafe-Common tools, cmake modules and the          #
+#    maidsafe_common libs and headers.                                         #
+#                                                                              #
+#  Settable variables to aid with finding MaidSafe-Common are:                 #
+#    MAIDSAFE_COMMON_INSTALL_DIR                                               #
+#                                                                              #
+#  If found, a target named maidsafe_common_static is imported.                #
+#                                                                              #
+#  Variables set and cached by this module are:                                #
+#    MaidSafeCommon_INCLUDE_DIR, MaidSafeCommon_MODULES_DIR,                   #
+#    MaidSafeCommon_TOOLS_DIR and MAIDSAFE_COMMON_VERSION.                     #
+#                                                                              #
+#==============================================================================#
 
 UNSET(MaidSafeCommon_INCLUDE_DIR CACHE)
 UNSET(MaidSafeCommon_MODULES_DIR CACHE)
 UNSET(MaidSafeCommon_TOOLS_DIR CACHE)
-UNSET(MaidSafeCommon_VERSION CACHE)
 
-IF(NOT MAIDSAFE_COMMON_INSTALL_DIR AND DEFAULT_THIRD_PARTY_ROOT)
-  SET(MAIDSAFE_COMMON_INSTALL_DIR ${DEFAULT_THIRD_PARTY_ROOT})
+IF(NOT MAIDSAFE_COMMON_INSTALL_DIR)
+  IF(DEFAULT_THIRD_PARTY_ROOT)
+    SET(MAIDSAFE_COMMON_INSTALL_DIR ${DEFAULT_THIRD_PARTY_ROOT})
+  ELSE()
+    SET(MAIDSAFE_COMMON_INSTALL_DIR ${PROJECT_SOURCE_DIR}/../MaidSafe-Common/installed)
+  ENDIF()
 ENDIF()
 
 SET(MAIDSAFE_PATH_SUFFIX share/maidsafe)
 FIND_FILE(MAIDSAFE_THIRD_PARTY_CMAKE maidsafe_third_party.cmake PATHS ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
 FIND_FILE(BOOST_LIBS_CMAKE boost_libs.cmake PATHS ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
 FIND_FILE(MAIDSAFE_COMMON_CMAKE maidsafe_common.cmake PATHS ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-FIND_FILE(MAIDSAFE_ENCRYPT_CMAKE encrypt.cmake PATHS ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-IF(MAIDSAFE_THIRD_PARTY_CMAKE AND BOOST_LIBS_CMAKE AND MAIDSAFE_COMMON_CMAKE AND MAIDSAFE_ENCRYPT_CMAKE)
+IF(MAIDSAFE_THIRD_PARTY_CMAKE AND BOOST_LIBS_CMAKE AND MAIDSAFE_COMMON_CMAKE)
   INCLUDE(${MAIDSAFE_THIRD_PARTY_CMAKE})
   INCLUDE(${BOOST_LIBS_CMAKE})
   INCLUDE(${MAIDSAFE_COMMON_CMAKE})
-  INCLUDE(${MAIDSAFE_ENCRYPT_CMAKE})
 ENDIF()
 
 SET(MAIDSAFE_PATH_SUFFIX include)
 FIND_PATH(MaidSafeCommon_INCLUDE_DIR maidsafe/common/version.h PATHS ${MAIDSAFE_COMMON_INC_DIR} ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-
-SET(MAIDSAFE_PATH_SUFFIX include/maidsafe)
-FIND_PATH(MaidSafeEncrypt_INCLUDE_DIR maidsafe-encrypt/version.h PATHS ${MAIDSAFE_COMMON_INC_DIR} ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
 
 SET(MAIDSAFE_PATH_SUFFIX share/maidsafe/cmake_modules)
 FIND_PATH(MaidSafeCommon_MODULES_DIR maidsafe_run_protoc.cmake PATHS ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
@@ -55,7 +71,7 @@ SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${MaidSafeCommon_MODULES_DIR})
 SET(MAIDSAFE_PATH_SUFFIX ../../..)
 FIND_PATH(DEFAULT_THIRD_PARTY_ROOT README PATHS ${MAIDSAFE_COMMON_INSTALL_DIR} PATH_SUFFIXES ${MAIDSAFE_PATH_SUFFIX} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
 
-IF(NOT MaidSafeCommon_INCLUDE_DIR OR NOT MaidSafeEncrypt_INCLUDE_DIR OR NOT MaidSafeCommon_MODULES_DIR OR NOT MaidSafeCommon_TOOLS_DIR)
+IF(NOT MaidSafeCommon_INCLUDE_DIR OR NOT MaidSafeCommon_MODULES_DIR OR NOT MaidSafeCommon_TOOLS_DIR)
   SET(ERROR_MESSAGE "${MaidSafeCommon_INCLUDE_DIR}\n")
   SET(ERROR_MESSAGE "${ERROR_MESSAGE}${MaidSafeCommon_MODULES_DIR}\n")
   SET(ERROR_MESSAGE "${ERROR_MESSAGE}${MaidSafeCommon_TOOLS_DIR}\n")
@@ -66,17 +82,5 @@ IF(NOT MaidSafeCommon_INCLUDE_DIR OR NOT MaidSafeEncrypt_INCLUDE_DIR OR NOT Maid
   MESSAGE(FATAL_ERROR "${ERROR_MESSAGE}")
 ENDIF()
 
-FILE(STRINGS ${MaidSafeCommon_INCLUDE_DIR}/maidsafe/common/version.h MaidSafeCommon_VERSION
-       REGEX "VERSION [0-9]+$")
-STRING(REGEX MATCH "[0-9]+$" MaidSafeCommon_VERSION ${MaidSafeCommon_VERSION})
-FILE(STRINGS ${MaidSafeEncrypt_INCLUDE_DIR}/maidsafe-encrypt/version.h MaidSafeEncrypt_VERSION
-       REGEX "ENCRYPT_VERSION [0-9]+$")
-
-STRING(REGEX MATCH "[0-9]+$" MaidSafeCommon_VERSION ${MaidSafeCommon_VERSION})
-STRING(REGEX MATCH "[0-9]+$" MaidSafeEncrypt_VERSION ${MaidSafeEncrypt_VERSION})
-INCLUDE_DIRECTORIES(${MaidSafeCommon_INCLUDE_DIR} ${MaidSafeEncrypt_INCLUDE_DIR})
-
-MESSAGE("-- Found MaidSafe Common library (version ${MaidSafeCommon_VERSION})")
-MESSAGE("-- Found MaidSafe Common Debug library (version ${MaidSafeCommon_VERSION})")
-MESSAGE("-- Found MaidSafe Encrypt library (version ${MaidSafeEncrypt_VERSION})")
-MESSAGE("-- Found MaidSafe Encrypt Debug library (version ${MaidSafeEncrypt_VERSION})")
+MESSAGE("-- Found MaidSafe Common library (version ${MAIDSAFE_COMMON_VERSION})")
+MESSAGE("-- Found MaidSafe Common Debug library (version ${MAIDSAFE_COMMON_VERSION})")
