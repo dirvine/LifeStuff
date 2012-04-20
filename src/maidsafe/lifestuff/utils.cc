9225ca0312bde3db77afad237cdfed9641de2b65
Steve Muecklisch <steve.muecklisch@maidsafe.net>
2012-04-20 20:41:35 +0100

    Adapted to change in PD's ClientContainer.

--- src/maidsafe/lifestuff/utils.cc
+++ src/maidsafe/lifestuff/utils.cc
@@ -291,6 +291,8 @@ int RetrieveBootstrapContacts(const fs::path &download_dir,
     bai::tcp::resolver resolver(io_service);
 //    bai::tcp::resolver::query query("96.126.103.209", "http");
     bai::tcp::resolver::query query("192.168.1.113", "http");
+//    bai::tcp::resolver::query query("192.168.1.119", "http");
+//    bai::tcp::resolver::query query("127.0.0.1", "http");
     bai::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 
     // Try each endpoint until we successfully establish a connection.
@@ -386,14 +388,13 @@ ClientContainerPtr SetUpClientContainer(
   }
 
   std::vector<dht::Contact> bootstrap_contacts;
-  int result = RetrieveBootstrapContacts(base_dir,
-                                         &bootstrap_contacts);
+  int result = RetrieveBootstrapContacts(base_dir, &bootstrap_contacts);
   if (result != kSuccess) {
     DLOG(ERROR) << "Failed to retrieve bootstrap contacts.  Result: " << result;
     return ClientContainerPtr();
   }
 
-  result = client_container->Start(&bootstrap_contacts);
+  result = client_container->Start(bootstrap_contacts);
   if (result != kSuccess) {
     DLOG(ERROR) << "Failed to start client_container.  Result: " << result;
     return ClientContainerPtr();
