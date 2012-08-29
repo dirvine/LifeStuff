50129c0ebd0f6bfabd9d18da5b9090ac68deaf77
Dan Schmidt <dan.schmidt@maidsafe.net>
2012-08-29 12:34:43 +0100

    Changes to run tests that mount two drives at the same time to run only on Linux, since MAC and Windows can't cope. Shocking.

--- src/maidsafe/lifestuff/tests/api/api_public_id_test.cc
+++ src/maidsafe/lifestuff/tests/api/api_public_id_test.cc
@@ -200,8 +200,8 @@ TEST_F(TwoUsersApiTest, FUNC_CreateSamePublicIdConsecutively) {
   EXPECT_EQ(kSuccess, test_elements_2_.LogOut());
 }
 
-TEST_F(TwoUsersApiTest, DISABLED_FUNC_CreateSamePublicIdSimultaneously) {
-#ifndef MAIDSAFE_APPLE
+TEST_F(TwoUsersApiTest, FUNC_CreateSamePublicIdSimultaneously) {
+#ifdef MAIDSAFE_LINUX
   EXPECT_EQ(kSuccess, test_elements_1_.LogIn(keyword_1_, pin_1_, password_1_));
   EXPECT_EQ(kSuccess, test_elements_2_.LogIn(keyword_2_, pin_2_, password_2_));
 
