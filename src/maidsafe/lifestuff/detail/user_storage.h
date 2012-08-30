50788b3d2d135d99b0087020538b250bb0f2c115
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2012-08-30 03:32:10 +0100

    Fixes for Windows builds.

--- src/maidsafe/lifestuff/detail/user_storage.h
+++ src/maidsafe/lifestuff/detail/user_storage.h
@@ -24,6 +24,7 @@
 
 #include <list>
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -31,6 +32,7 @@
 #include "boost/asio/io_service.hpp"
 #include "boost/thread/condition_variable.hpp"
 #include "boost/thread/mutex.hpp"
+#include "boost/thread/thread.hpp"
 
 #ifdef WIN32
 #  include "maidsafe/drive/win_drive.h"
