831ccd93f6c8644423f22091b968b6d6c2de3f74
Steve Muecklisch <steve.muecklisch@maidsafe.net>
2012-11-28 13:21:57 +0000

    Created Python API bindings for LifeStuff class, added as executable target.

---
+++ src/maidsafe/lifestuff/python_bindings/lifestuff_python_api.cc
@@ -0,0 +1,84 @@
+/*
+* ============================================================================
+*
+* Copyright [2012] maidsafe.net limited
+*
+* Description:  Exposing the LifeStuff API as a module to Python.
+* Version:      1.0
+* Created:      2012-11-28
+* Revision:     none
+* Compiler:     gcc
+* Company:      maidsafe.net limited
+*
+* The following source code is property of maidsafe.net limited and is not
+* meant for external use.  The use of this code is governed by the license
+* file LICENSE.TXT found in the root of this directory and also on
+* www.maidsafe.net.
+*
+* You are not free to copy, amend or otherwise use this source code without
+* the explicit written permission of the board of directors of maidsafe.net.
+*
+* ============================================================================
+*/
+
+#include "boost/filesystem/path.hpp"
+#include "boost/python.hpp"
+
+#include "maidsafe/lifestuff/lifestuff_api.h"
+
+namespace ls = maidsafe::lifestuff;
+
+BOOST_PYTHON_MODULE(lifestuff_api) {
+  boost::python::class_<ls::LifeStuff>(
+      "LifeStuff", boost::python::init<ls::Slots, boost::filesystem::path>())
+
+      // Credential operations
+      .def("CreateUser", &ls::LifeStuff::CreateUser)
+      .def("CreatePublicId", &ls::LifeStuff::CreatePublicId)
+      .def("LogIn", &ls::LifeStuff::LogIn)
+      .def("LogOut", &ls::LifeStuff::LogOut)
+      .def("MountDrive", &ls::LifeStuff::MountDrive)
+      .def("UnMountDrive", &ls::LifeStuff::UnMountDrive)
+      .def("StartMessagesAndIntros", &ls::LifeStuff::StartMessagesAndIntros)
+      .def("StopMessagesAndIntros", &ls::LifeStuff::StopMessagesAndIntros)
+      .def("CheckPassword", &ls::LifeStuff::CheckPassword)
+      .def("ChangeKeyword", &ls::LifeStuff::ChangeKeyword)
+      .def("ChangePin", &ls::LifeStuff::ChangePin)
+      .def("ChangePassword", &ls::LifeStuff::ChangePassword)
+//       .def("ChangePublicId", &ls::LifeStuff::ChangePublicId)
+      .def("LeaveLifeStuff", &ls::LifeStuff::LeaveLifeStuff)
+
+      // Contact operations
+      .def("AddContact", &ls::LifeStuff::AddContact)
+      .def("ConfirmContact", &ls::LifeStuff::ConfirmContact)
+      .def("DeclineContact", &ls::LifeStuff::DeclineContact)
+      .def("RemoveContact", &ls::LifeStuff::RemoveContact)
+      .def("ChangeProfilePicture", &ls::LifeStuff::ChangeProfilePicture)
+      .def("GetOwnProfilePicture", &ls::LifeStuff::GetOwnProfilePicture)
+      .def("GetContactProfilePicture", &ls::LifeStuff::GetContactProfilePicture)
+      .def("GetLifestuffCard", &ls::LifeStuff::GetLifestuffCard)
+      .def("SetLifestuffCard", &ls::LifeStuff::SetLifestuffCard)
+      .def("GetContacts", &ls::LifeStuff::GetContacts)
+      .def("PublicIdsList", &ls::LifeStuff::PublicIdsList)
+
+      // Messaging
+      .def("SendChatMessage", &ls::LifeStuff::SendChatMessage)
+      .def("SendFile", &ls::LifeStuff::SendFile)
+      .def("AcceptSentFile", &ls::LifeStuff::AcceptSentFile)
+      .def("RejectSentFile", &ls::LifeStuff::RejectSentFile)
+
+      // Filesystem
+      .def("ReadHiddenFile", &ls::LifeStuff::ReadHiddenFile)
+      .def("WriteHiddenFile", &ls::LifeStuff::WriteHiddenFile)
+      .def("DeleteHiddenFile", &ls::LifeStuff::DeleteHiddenFile)
+      .def("SearchHiddenFiles", &ls::LifeStuff::SearchHiddenFiles)
+
+      // getters
+      .def("state", &ls::LifeStuff::state)
+      .def("logged_in_state", &ls::LifeStuff::logged_in_state)
+      .def("mount_path", &ls::LifeStuff::mount_path);
+}
+
+int main() {
+  return 0;
+}
