fc7d2985220e34c0b6061cbed566a416a3702b6f
Brian Smith <brian.smith@maidsafe.net>
2013-06-18 18:08:37 +0100

    Some header documentation.

--- include/maidsafe/lifestuff/lifestuff_api.h
+++ include/maidsafe/lifestuff/lifestuff_api.h
@@ -19,6 +19,14 @@
 namespace maidsafe {
 namespace lifestuff {
 
+// LifeStuff provides a convenient interface for client applications wishing to make use of the
+// novinet network, http://novinet.com/. Further details and links can be found at
+// http://maidsafe.github.io/LifeStuff/. Secure methods for managing user input are provided. Input
+// types Keyword, Pin and Password defined in the MaidSafe-Passport project store encrypted
+// strings until plaintext is required, whereupon they are converted to safe strings. Safe strings
+// come with operating system dependent non-paging guarantees and overwrite their memory during
+// destruction.
+
 class LifeStuffImpl;
 
 class LifeStuff {
@@ -26,9 +34,16 @@ class LifeStuff {
   LifeStuff(const Slots& slots);
   ~LifeStuff();
 
+  // Creates and/or inserts a string of 'characters' at position 'position' in the input string,
+  // keyword, pin, password, etc., determined by 'input_field', see LifeStuff.h for the
+  // InputField definition. Implicitly accepts Unicode characters converted to std::string.
   void InsertUserInput(uint32_t position, const std::string& characters, InputField input_field);
+  // Removes the sequence of characters starting at position 'position' and ending at position
+  // 'position' + 'length' from the input string determined by 'input_field'.
   void RemoveUserInput(uint32_t position, uint32_t length, InputField input_field);
+  // Clears the currently inserted characters from the input string determined by 'input_field'.
   void ClearUserInput(InputField input_field);
+  // Confirms pairs, password/confirmation password, etc., of input strings for equality.
   bool ConfirmUserInput(InputField input_field);
 
   void CreateUser(const std::string& vault_path, ReportProgressFunction& report_progress);
